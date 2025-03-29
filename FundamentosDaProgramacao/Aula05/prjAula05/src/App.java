3fff5e96e8e6b2f25b90fddc9ed26a3b18c7b5e6
PedroMachadoGRP <143435304+PedroMachadoGRP@users.noreply.github.com>
2025-03-28 22:00:10 -0300

    Add files via upload

---
+++ FundamentosDaProgramacao/Aula05/prjAula05/src/App.java
@@ -0,0 +1,91 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+    Scanner scanner = new Scanner(System.in);
+        System.out.println("");
+    /* 
+    System.out.println("Imprimindo a  tabuada do 5...");
+
+    for (int i = 1; i <= 10; i++) {
+        System.out.println("5 * " + i + " = " + (5*i));
+        }
+    for (int i = 10; i >= 1; i--) {
+            System.out.println("5 * " + i + " = " + (5*i));
+            }
+
+
+
+    for (int i = 0; i <= 10000; i++) {
+            System.out.println(i);
+            for (int j = 0; j <= 10000; j++) {
+                System.out.println("Percorrendo a linha["+i+"] coluna[" +j + "]");
+                }
+            }
+    
+
+    int i = 0;
+    while (i< 5) {
+        System.out.println("Valor de i:" + i);
+        i++;
+    }*/
+    
+    /* 
+    int i = 0;
+    int num = 5;
+    while (i <= 10) {
+        System.out.println(num + " * " + i + " = " + (num * i));
+        i++;
+    }
+    */
+
+    /* 
+    int i = 10;
+    int num = 5;
+    while (i >= 0) {
+        System.out.println(num + " * " + i + " = " + (num * i));
+        i--;
+    }*/
+
+    /* 
+    for (int i = 0; i < 10; i++) {
+        if(i == 4){
+            continue;
+        }
+        
+        System.out.println("Valor de i: "+ i);
+    }
+    */
+    
+    /* 
+    for (int i = 0; i <= 10; i++) {
+            if(i == 7){
+                break;
+            }
+            System.out.println("Valor de i: " +  i);
+        }
+        */
+        
+    /*Exercicio 1
+    System.out.println("Digite o número que você deseja saber a tabuada até o 10");
+    int num = scanner.nextInt();
+
+    for(int i = 1; i <= 10; i++){
+        System.out.println(num + " * " + i + " = " + (num * i) + "\n");
+    }*/
+
+    
+    /*Exercicio2
+    System.out.println("");
+    System.out.println("Digite o número que você deseja saber a tabuada até o 10");
+    int num = scanner.nextInt(); 
+    
+    int i = 10;
+    while (i >= 1) {
+        System.out.println(num + " * " + i + " = " + (num * i));
+        i--;
+    }*/
+
+
+    }
+}
