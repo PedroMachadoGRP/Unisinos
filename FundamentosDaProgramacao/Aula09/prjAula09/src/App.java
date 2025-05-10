6642e95927b1c042a0f72316bf665448c1661324
PedroMachadoGRP <143435304+PedroMachadoGRP@users.noreply.github.com>
2025-05-09 21:42:39 -0300

    Add files via upload

---
+++ FundamentosDaProgramacao/Aula09/prjAula09/src/App.java
@@ -0,0 +1,76 @@
+import java.util.Random;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println(" ");
+
+        /* 
+        //Array de inteiros com 10 posições
+        int[] arValores = new int[10];
+        int[] arValores2 = new int[10];
+        int[] resultado = new int[10];
+        int[] arInveso = new int[10];
+        
+
+        for (int i = 0; i < arValores.length; i++) {
+            arValores[i] = i + 3;
+            arValores2[i] = i + 7;
+            System.out.println(arValores[i]);
+
+            
+
+        }
+
+        for (int i = 0; i < arValores.length; i++) {
+            resultado[i] = arValores[i] + arValores2[i]; 
+            arInveso[i] =  arValores[i] + arValores2[9 - i];
+            
+
+            System.out.println("Resultado da soma: " + resultado[i] );
+            System.out.println(arInveso[i]);
+        }
+
+        */
+
+
+        /* 
+        int[] vetor1 = {10,20,30,40,50,60,70,80,90,100};
+        int[] vetor2 = new int[10];
+        Random gerador = new Random();
+
+        for (int i = 0; i < 10; i++) {
+            vetor2[i] = gerador.nextInt();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Somar Vetores: " + (vetor1[i] + vetor2[i]));
+            System.out.println("subtrair Vetores: " + (vetor1[i] - vetor2[i]));
+            System.out.println("Multiplicar Vetores: " + (vetor1[i] * vetor2[i]));
+        }
+            */
+
+            /* 
+            int[] vTeste = {1,2,3,4,5,6,7,8,9,10};
+            int pares = 0;
+            int impares = 0;
+        
+            for (int i = 0; i < 10; i++) {
+                if((vTeste[i]%2) == 0){
+                    pares++;
+                    impares++;
+                    System.out.println(pares);
+                    System.out.println(impares);
+                }
+                
+            }
+                */
+
+                /* 
+                int[] arPrimeiro = {1,2,3,4,5,6};
+                int[] arSegundo = {10,20,30,40,50,60};
+                int[] uniao = new int[12];
+                */
+
+
+    }
+}
