7afa2dcfc75ace1edcbd3991415d74efa9a3b1de
PedroMachadoGRP <143435304+PedroMachadoGRP@users.noreply.github.com>
2025-05-16 21:44:53 -0300

    Add files via upload

---
+++ FundamentosDaProgramacao/Aula10/prjAula10/src/App.java
@@ -0,0 +1,53 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("  ");
+        /* 
+        // Declaração e inicialização da matriz
+        int[][] matrizA = { 
+             {1, 2, 3, 4},
+             {5, 6, 7, 8}, 
+             {3, 9, 18, 10}, 
+             {5, 8, 7, 5}};
+        int linhas = matrizA.length;
+        int colunas = matrizA[0].length;
+        System.out.println("Quantidade de linhas: " + linhas);
+        System.out.println("Quantidade de colunas: " + colunas);
+        // Declaração da matriz (Quantidade de linhas e colunas)
+        int[][] matrizB = new int[4][4];
+        // Inserção dos valores na matriz
+        // 1o for - Percorrer as linhas
+        for (int i = 0; i < 4; i++) {
+            // 2o for - Percorrer as colunas
+            for (int j = 0; j < 4; j++) {
+                // Atribuir os valores a cada uma das posições
+                matrizB[i][j] = i + j;
+            }
+        }
+        // Imprimir a matriz
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++)
+                System.err.println("Matriz[" + i + "][" + j + "]: " + matrizB[i][j]);
+        }
+                */
+
+        /* Soma de matrizes
+            int[][] matrizA = {{1,2,3},{4,5,6},{7,8,9}};
+            int[][] matrizB = {{10,11,12},{13,14,15},{16,17,18}};
+            int[][] resultado = new int[3][3];
+
+            for (int i = 0; i < resultado.length; i++) {
+                for (int j = 0; j < resultado.length; j++) {
+                    resultado[i][j] = matrizA[i][j] + matrizB[i][j];
+                }
+            }
+
+            for (int i = 0; i < resultado.length; i++) {
+                for (int j = 0; j < resultado.length; j++) {
+                    System.out.println("Soma da matriz["+ i + "]["+j+"]: " + resultado[i][j]);
+                }
+            }
+                */
+
+                
+    }
+}
