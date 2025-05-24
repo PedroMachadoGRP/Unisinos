20a73720461e061cf629b27751e117583dd16245
PedroMachadoGRP <143435304+PedroMachadoGRP@users.noreply.github.com>
2025-05-23 21:57:24 -0300

    Add files via upload

---
+++ FundamentosDaProgramacao/Aula11/prjAula11/src/App.java
@@ -0,0 +1,62 @@
+import java.nio.channels.Pipe.SourceChannel;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println(" ");
+        /* 
+        String str1 = "Texto 'delimitado' por 'apostrofo'";
+        String str2 = "Texto \"delimitado\" por \"aspas\"";
+        String str3 = "Texto com quebra \n de linha ";
+
+        System.out.println(str1 + str2 + str3);
+
+        System.out.println(str1 + "\n" + str2 + "\n" + str3);
+        */
+
+        /* 
+        String strTexto = "Um texto qualquer...";
+
+        for (int i = 0; i < strTexto.length(); i++) {
+            System.out.println("Posição["+ i +"]: " +  strTexto.charAt(i));
+            
+        }
+        */
+
+        /* 
+        String strNome = "Mariana";
+        String strNome2 = "mariana";
+
+        if(strNome.equals(strNome2))
+                System.out.println("Os nome são iguais");
+        else
+        System.out.println("Os nomes não são iguais");
+        strNome = "";
+        strNome2 = "mariana".toUpperCase();
+
+        if(strNome.equals(strNome2))
+        System.out.println("Os nome são iguais");
+        else
+        System.out.println("Os nome não são iguais");
+*/
+  /* 
+        String _nome = "João Jose Bobrinho ";
+        String[] arnome = _nome.split(" ");
+    */
+
+        /* 
+        String strNomeCompleto = "Pedro Antonio da Silva";
+        int idade = 18;
+
+        System.out.println(String.format("Eu sou %s e tenho %d anos",strNomeCompleto,idade));
+        */
+
+        /* 
+        String nome = "Antônio";
+        String nomeMeio = " de Almeida";
+        String ultimoNome = " e Alcantra";
+
+        System.out.println(nome + nomeMeio + ultimoNome);
+        System.out.println(nome.concat(nomeMeio).concat(ultimoNome));
+        */
+    }
+}
